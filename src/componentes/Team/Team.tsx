c8081b7821164ca69f76eb6ba52bb16628ab98da
Albert2707 <albertjohan2707@gmail.com>
2024-04-12 23:02:39 -0400

    deploy

--- src/componentes/Team/Team.tsx
+++ src/componentes/Team/Team.tsx
@@ -1,65 +1,75 @@
-import "./Team.scss"
+import { useContext, useEffect, useRef } from "react";
+import "./Team.scss";
+import { useInView } from "framer-motion";
+import { scrollContext } from "../../context/scrollContext";
+import { ContextProps } from "../../context/type";
 const Team = () => {
+  const { SetsectionChange } = useContext(scrollContext) as ContextProps;
 
-  /**        Albert Joan Agramonte Suero
-        Darian Anderson King Arias - 1125022
-        Erick Vásquez 1122627 */
+  const teamRef = useRef(null);
+  const isInView = useInView(teamRef, {
+    amount:"all"
+  });
+  useEffect(() => {
+    SetsectionChange(isInView ? true : false);
+  }, [isInView]);
+
   return (
     <section className="team">
-
-      <div className="container">
+      <div className="container" ref={teamRef}>
         <div className="teamTitle">
-          <span>
-            Our Team
-          </span>
+          <span>Our Team</span>
         </div>
         <div className="wrapper">
           <div className="card">
             <div className="image">
-              <img src="https://images.pexels.com/photos/20511145/pexels-photo-20511145/free-photo-of-a-woman-with-long-hair-and-a-white-sweater.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+              <img
+                src="https://images.pexels.com/photos/20511145/pexels-photo-20511145/free-photo-of-a-woman-with-long-hair-and-a-white-sweater.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
             </div>
             <div className="memberInfo">
               <div className="top">
-
                 <span className="memberName">Albert Joan Agramonte</span>
                 <span className="memberId">1225332</span>
               </div>
               <span className="memberCareer">Software engineering</span>
             </div>
-
           </div>
           <div className="card">
             <div className="image">
-              <img src="https://images.pexels.com/photos/20511145/pexels-photo-20511145/free-photo-of-a-woman-with-long-hair-and-a-white-sweater.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+              <img
+                src="https://images.pexels.com/photos/20511145/pexels-photo-20511145/free-photo-of-a-woman-with-long-hair-and-a-white-sweater.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
             </div>
             <div className="memberInfo">
               <div className="top">
-
                 <span className="memberName">Darian Anderson King Arias</span>
                 <span className="memberId">1125022</span>
               </div>
               <span className="memberCareer">System engineering</span>
             </div>
-
           </div>
           <div className="card">
             <div className="image">
-              <img src="https://images.pexels.com/photos/20511145/pexels-photo-20511145/free-photo-of-a-woman-with-long-hair-and-a-white-sweater.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+              <img
+                src="https://images.pexels.com/photos/20511145/pexels-photo-20511145/free-photo-of-a-woman-with-long-hair-and-a-white-sweater.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
             </div>
             <div className="memberInfo">
               <div className="top">
-
                 <span className="memberName">Erick Vásquez</span>
                 <span className="memberId">1122627</span>
               </div>
               <span className="memberCareer">Software engineering</span>
             </div>
-
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Team
+export default Team;
